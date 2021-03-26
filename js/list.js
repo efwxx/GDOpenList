fc5bca59105302102fd98b3216805dac58b38837
Res <57029509+resyal@users.noreply.github.com>
2021-03-25 21:13:26 -0400

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,115 +1,249 @@
 const d = {
-	"list": [
-    /*=================================================================================*/
-		
-    {
-		"vids": [
-        {
-					"user": "",
-					"link": "",
-					"percent": "",
-					"hz": "1000FPS"
-				},
-			],
-                       "name": "Wistfully",
-                       "author": "xViola",
-                       "more": "KrXyt",
-                       "id": 67344125,
-                       "pass": "557774",
-                       "percentToQualify": "5",
-                       "verificationVid": "https://youtu.be/a4uVJWF-it4",
-                       "key": 0
-                },
-	
-  /*=================================================================================*/
-  
-  {
-		"vids": [
-        {
-					"user": "",
-					"link": "",
-					"percent": "",
-					"hz": "3000 FPS"
-				},
-			],
-                       "name": "Singularity",
-                       "author": "070",
-                       "more": "070",
-                       "id": 0,
-                       "pass": "",
-                       "percentToQualify": "5",
-                       "verificationVid": "https://youtu.be/ZpeEs5PNAyQ?t=191",
-                       "key": 1
-                },
-  
-  /*=================================================================================*/
-  
-  
-  {
-		"vids": [
-        {
-					"user": "",
-					"link": "",
-					"percent": "",
-					"hz": "1440 FPS"
-				},
-			],
-                       "name": "Arcturus",
-                       "author": "maxfs",
-                       "more": "maxfs",
-                       "id": 0,
-                       "pass": "",
-                       "percentToQualify": "5",
-                       "verificationVid": "https://youtu.be/Wi9ImtkDPG8",
-                       "key": 2
-                },
-  
-  /*=================================================================================*/
-  {
-		"vids": [
-        {
-					"user": "",
-					"link": "",
-					"percent": "",
-					"hz": "720 FPS"
-				},
-			],
-                       "name": "Silent Circles 15",
-                       "author": "TheRealSailent",
-                       "more": "070",
-                       "id": 0,
-                       "pass": "pass",
-                       "percentToQualify": "5",
-                       "verificationVid": "https://www.youtube.com/watch?v=XfhxMBwJo7A",
-                       "key": 3
-                },
-   /*=================================================================================*/
-   {
-		"vids": [
-        {
-					"user": "",
-					"link": "",
-					"percent": "",
-					"hz": "1872 FPS"
-				},
-			],
-                       "name": "MINUSclub",
-                       "author": "xVOLUMNIUS",
-                       "more": "xVOLUMNIUS",
-                       "id": 0,
-                       "pass": "pass",
-                       "percentToQualify": "5",
-                       "verificationVid": "https://youtu.be/1SdZjtUaqV8",
-                       "key": 4
-                },
-   /*=================================================================================*/
+    "list": [
+        /*=================================================================================*/
 
-  ],
-	"version": [
-		2,
-		0, 
-		0
-	]
+        {
+            "vids": [
+                {
+                    "user": "",
+                    "link": "",
+                    "percent": "n/a",
+                    "hz": "1000FPS"
+                },
+            ],
+            "name": "Wistfully",
+            "author": "xViola",
+            "more": "KrXyt",
+            "id": 67344125,
+            "pass": "557774",
+            "percentToQualify": "5",
+            "verificationVid": "https://youtu.be/a4uVJWF-it4",
+            "key": 0
+        },
+
+        /*=================================================================================*/
+
+        {
+            "vids": [
+                {
+                    "user": "HaydenDom",
+                    "link": "https://youtu.be/agU2JBq6F00",
+                    "percent": "32",
+                    "hz": "3000 FPS"
+                },
+            ],
+            "name": "Singularity",
+            "author": "070",
+            "more": "070",
+            "id": 67432669,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://youtu.be/ZpeEs5PNAyQ?t=191",
+            "key": 1
+        },
+
+        /*=================================================================================*/
+
+
+        {
+            "vids": [
+                {
+                    "user": "Hyperbola",
+                    "link": "https://cdn.discordapp.com/attachments/730372415154159657/813811561600647228/aunwr.mp4",
+                    "percent": "5",
+                    "hz": "1440 FPS"
+                },
+            ],
+            "name": "Arcturus",
+            "author": "maxfs",
+            "more": "Wuro",
+            "id": 67059113,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://youtu.be/Wi9ImtkDPG8",
+            "key": 2
+        },
+
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "UFokinWotM8",
+                    "link": "https://www.youtube.com/watch?v=SaQacnJmlpo",
+                    "percent": "31",
+                    "hz": "720 FPS"
+                },
+            ],
+            "name": "Silent Circles 15",
+            "author": "TheRealSailent",
+            "more": "070",
+            "id": 66634469,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://www.youtube.com/watch?v=XfhxMBwJo7A",
+            "key": 3
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "",
+                    "link": "",
+                    "percent": "n/a",
+                    "hz": "1872 FPS"
+                },
+            ],
+            "name": "MINUSclub",
+            "author": "xVOLUMNIUS",
+            "more": "xVOLUMNIUS",
+            "id": 65476277,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://youtu.be/1SdZjtUaqV8",
+            "key": 4
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "",
+                    "link": "",
+                    "percent": "n/a",
+                    "hz": "720 FPS"
+                },
+            ],
+            "name": "VOIDclub",
+            "author": "Hyperbola",
+            "more": "Hyperbola",
+            "id": "N/A (Unreleased)",
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://youtu.be/342pFNevEAs",
+            "key": 5
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "Glitchi",
+                    "link": "https://www.youtube.com/watch?v=mguwtdt9hjg&t=110s",
+                    "percent": "5",
+                    "hz": "720 FPS"
+                },
+            ],
+            "name": "Silentium Excelsus",
+            "author": "AuraXalaiv",
+            "more": "AuraXalaiv",
+            "id": 66381110,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://www.youtube.com/watch?v=w6aYnX0e6Lc",
+            "key": 6
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "",
+                    "link": "",
+                    "percent": "n/a",
+                    "hz": "1225 FPS"
+                },
+            ],
+            "name": "Sad Santa",
+            "author": "070",
+            "more": "070",
+            "id": 66424814,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://www.youtube.com/watch?v=w6aYnX0e6Lc",
+            "key": 7
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "",
+                    "link": "",
+                    "percent": "n/a",
+                    "hz": "720 FPS"
+                },
+            ],
+            "name": "Rampant Circles",
+            "author": "NXISE",
+            "more": "TomScott",
+            "id": 66357460,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://www.youtube.com/watch?v=rG5xdXQL79I",
+            "key": 8
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "maxfs",
+                    "link": "https://youtu.be/hhAKd6lx0Zs",
+                    "percent": "6",
+                    "hz": "720 FPS"
+                },
+            ],
+            "name": "lastheory",
+            "author": "Maxfs",
+            "more": "Wuro",
+            "id": 67645567,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://youtu.be/VXPY_tKp4rA",
+            "key": 9
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "",
+                    "link": "",
+                    "percent": "n/a",
+                    "hz": "900 FPS"
+                },
+            ],
+            "name": "Kingdom Of Miracalis",
+            "author": "KrXyt",
+            "more": "KrXyt",
+            "id": 64075068,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://youtu.be/RtCQ1LQAbO0",
+            "key": 10
+        },
+        /*=================================================================================*/
+        {
+            "vids": [
+                {
+                    "user": "",
+                    "link": "",
+                    "percent": "n/a",
+                    "hz": "720 FPS"
+                },
+            ],
+            "name": "Radiation Pressure",
+            "author": "Convindix",
+            "more": "KrXyt",
+            "id": 67222753,
+            "pass": "",
+            "percentToQualify": "5",
+            "verificationVid": "https://www.youtube.com/watch?v=bPO6bBwqLPQ",
+            "key": 11
+        },
+        /*=================================================================================*/
+        /*=================================================================================*/
+
+    ],
+    "version": [
+        2,
+        0,
+        0
+    ]
 };
 /*bottom right*/
-const list = d.list;const version = d.version;
+const list = d.list; const version = d.version;
